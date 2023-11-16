9e802ef1986c579657192a64fc655c395a71a6fc
usysrc <usysrc@usysrc.dev>
2023-11-16 20:15:18 +0100

    added database to start script

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
+
+# start the database
+docker compose up postgres -d --wait
+
+# stop the database when this script exits
+trap 'docker compose down' EXIT
+
+# migrate the database
+go run cmd/migrate/main.go
+
+# start the server
 air --build.cmd "go build -o ./bin/api ./cmd/app/" --build.bin "./bin/api"
